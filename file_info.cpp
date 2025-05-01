cc7a7dca51b21fc2cfd9c532efb2ac0545d52ae9
bhargava <bhargavaa2655@gmail.com>
2025-05-01 17:29:46 +0530

    Updated the code with MULTITHREADING to maintian parallelization

--- file_info.cpp
+++ file_info.cpp
@@ -3,13 +3,16 @@
 #include <chrono>
 #include <ctime>
 #include <iomanip>
+#include <thread>
+#include <mutex>
                                             // \(ASCII value)=92
 namespace fs = std::filesystem;             // C:\Windows\System32
 
 class SysInfo{
     public:
-    void Describe(const std::string& dir){
+    void Describe(const std::string& dir,std::mutex& mtx){
         try{
+            std::lock_guard<std::mutex> lock(mtx); //synchronize output to console
             if(fs::is_directory(dir)){
                 for (const auto &entry : fs::directory_iterator(dir)){
                     std::cout << entry.path() <<std::endl;
@@ -21,26 +24,30 @@ class SysInfo{
             std::cerr<<"Error : "<<e.what()<<std::endl;
         }
     }
-    float sizeMB(const std::string& path){
+    float sizeMB(const std::string& path,std::mutex& mtx){
         try{
             auto sizeMB=fs::file_size(path)/(1024.0f*1024.0f);
-            return sizeMB;
+            std::lock_guard<std::mutex> lock(mtx);  //synchronize output to console
+            std::cout<<"File size: "<<sizeMB<<" MB"<<std::endl;
+            //return sizeMB;
         }catch(const fs::filesystem_error &e){
             std::cerr<<"Error : "<<e.what()<<std::endl;
         }
         return 0.0f;
     }
-    float sizeGB(const std::string& path){
+    float sizeGB(const std::string& path,std::mutex& mtx){
         try{
             auto sizeGB=fs::file_size(path)/(1024.0f*1024.0f*1024.0f);
-            return sizeGB;
+            std::lock_guard<std::mutex> lock(mtx);  // Synchronize output to console 
+            std::cout<<"File size: "<<sizeGB<<" GB"<<std::endl;
+            //return sizeGB;
         }catch(fs::filesystem_error &e){
             std::cerr<<"Error : "<<e.what()<<std::endl;
         }
         return 0.0f;
     }
     
-    void Modification_details(const std::string& path){
+    void Modification_details(const std::string& path,std::mutex& mtx){
         try{
             auto ftime=fs::last_write_time(path);
 
@@ -48,33 +55,50 @@ class SysInfo{
             auto sctp=std::chrono::time_point_cast<std::chrono::system_clock::duration>(ftime-fs::file_time_type::clock::now()+std::chrono::system_clock::now());
             
             std::time_t cftime=std::chrono::system_clock::to_time_t(sctp);
+            std::lock_guard<std::mutex> lock(mtx); 
             std::cout<<"Last modified: "<<std::asctime(std::localtime(&cftime));
         }catch(fs::filesystem_error &e){
             std::cerr<<"Error : "<<e.what()<<std::endl;
         }
     }
 
-    void Extension(const std::string& loc){
+    void Extension(const std::string& loc,std::mutex& mtx){
         //fs::path is an object representing a file or directory path
 
         fs::path p(loc);
         auto ext=p.extension();
+        std::lock_guard<std::mutex> lock(mtx);
         std::cout<<"File extension : "<<ext<<std::endl;
     }
 };
+
+void runParallel(SysInfo& obj,const std::string& path){
+    std::mutex mtx;
+
+    std::thread t1(&SysInfo::Describe,&obj,path,std::ref(mtx));
+    std::thread t2(&SysInfo::sizeMB,&obj,path,std::ref(mtx));
+    std::thread t3(&SysInfo::sizeGB,&obj,path,std::ref(mtx));
+    std::thread t4(&SysInfo::Modification_details,&obj,path,std::ref(mtx));
+    std::thread t5(&SysInfo::Extension,&obj,path,std::ref(mtx));
+
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+    t5.join();
+}
+
+
 int main() {
     SysInfo obj;
-    std::string dir;/*"C:\\Users\\bharg\\Videos\\DL-videos\\15 Backpropagation in Deep Learning _ Part 1 _ The What_.mp4";*/
+    std::string dir; /* "C:\\Users\\bharg\\Videos\\DL-videos\\15 Backpropagation in Deep Learning _ Part 1 _ The What_.mp4"; */
     std::cout<<"Enter the file or directory path : ";
     std::getline(std::cin, dir);
     if(!fs::exists(dir)){
         std::cout<<"File not found.\n";
         return 1;
     }
-    obj.Describe(dir);
     std::cout<<std::fixed<<std::setprecision(2);
-    std::cout<<"file size in "<<obj.sizeMB(dir)<<"MB or "<<obj.sizeGB(dir)<<"GB"<<std::endl;
-    obj.Modification_details(dir);
-    obj.Extension(dir);
+    runParallel(obj,dir);
     return 0;
 }
